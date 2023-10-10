6fd0cf66121e755cdecc177519ea4b9df376adb0
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2023-10-10 11:05:30 -0600

    added lambda.

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,14 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_18_X,
+      code: Code.fromAsset('lambda'),
+      handler: 'hello.handler',
+    });
   }
 }
